e1ea9f4e4147cddbeee101c081004f4d1cee6bbb
whonore <whonore@u.rochester.edu>
2016-07-06 10:52:13 -0500

    Major redesign

---
+++ python/genalg.py
@@ -0,0 +1,117 @@
+from __future__ import division
+
+import copy
+
+import numpy as np
+
+
+class GenAlg(object):
+    EPS = 1e-4
+
+    def __init__(self, chrom_cls, pool_size=40, max_gen=None, select_ratio=0.7,
+                 progress=False, interval=20, **chrom_kwargs):
+        np.random.seed(0)
+
+        self.max_gen = max_gen
+        self.select_ratio = select_ratio
+        self.progress = progress
+        self.interval = interval
+
+        self.pool_size = pool_size
+        self.pool = np.array([chrom_cls(**chrom_kwargs)
+                              for _ in range(pool_size)])
+
+    def evolve(self):
+        gen = 0
+        select_size = np.floor(self.select_ratio * self.pool_size)
+        select_size += select_size % 2
+        select_size = int(min(select_size, self.pool_size))
+
+        while self.max_gen is None or gen < self.max_gen:
+            if self.progress and gen % self.interval == 0:
+                fits = np.array([chrom.fitness() for chrom in self.pool])
+                print("Generation {}".format(gen))
+                print("\tFitness min/max/mean {:.4f} {:.4f} {:.4f}"
+                      .format(fits.min(), fits.max(), fits.mean()))
+                best = self.pool.min()
+
+                best.log()
+
+            best, worst = GenAlg.get_children(self.pool, select_size)
+            children = []
+            for b1, b2 in np.split(best, select_size / 2):
+                c1 = Chromosone.crossover(b1, b2)
+                c2 = Chromosone.crossover(b2, b1)
+
+                c1.mutate()
+                c2.mutate()
+
+                children.append(c1)
+                children.append(c2)
+
+            for w, c in zip(worst, children):
+                w.replace(c)
+
+            fittest = self.pool.min()
+            if fittest.fitness() == 0 and fittest.valid:
+                return fittest
+
+            gen += 1
+
+    @staticmethod
+    def get_children(pool, num):
+        worst_fits = np.array([chrom.fitness() + GenAlg.EPS
+                               for chrom in pool])
+        best_fits = 1 / worst_fits
+
+        worst_fits /= sum(worst_fits)
+        best_fits /= sum(best_fits)
+
+        best = np.random.choice(pool, num, replace=True, p=best_fits)
+        worst = np.random.choice(pool, num, replace=True, p=worst_fits)
+
+        return best, worst
+
+
+class Chromosone(object):
+    def __init__(self, ngenes, gene_bits, crossover_rate=0.7,
+                 mutate_rate=0.001):
+        self.ngenes = ngenes
+        self.gene_bits = gene_bits
+
+        nbits = ngenes * gene_bits
+        nbytes = nbits // 8 + 1
+
+        rand_data = (np.random.rand(nbytes) * 255).astype('uint8')
+        self.genes = np.unpackbits(rand_data).astype('bool')[:nbits]
+
+        self.crossover_rate = crossover_rate
+        self.mutate_rate = mutate_rate
+
+        self._recalc = True
+        self._fit = None
+
+    def __lt__(self, other):
+        return self.fitness() < other.fitness()
+
+    @staticmethod
+    def crossover(left, right):
+        new = copy.deepcopy(left)
+
+        if np.random.random() <= left.crossover_rate:
+            new._recalc = True
+
+            start = np.random.randint(len(new.genes))
+            new.genes[start:] = right.genes[start:]
+
+        return new
+
+    def mutate(self):
+        for idx, bit in enumerate(self.genes):
+            if np.random.random() <= self.mutate_rate:
+                self._recalc = True
+                self.genes[idx] = not bit
+
+    def replace(self, other):
+        self.genes = other.genes
+        self._recalc = True
---
+++ python/image_evolve.py
@@ -0,0 +1,102 @@
+from __future__ import division
+
+import argparse
+import os
+import shutil
+
+import numpy as np
+from PIL import Image, ImageChops, ImageDraw, ImageStat
+
+import genalg as ga
+
+
+class ImageChromo(ga.Chromosone):
+    COLOR = {'num': 3, 'dtype': np.dtype('uint8')}
+    POS = {'num': 2, 'dtype': np.dtype('uint32')}
+    SIZE = {'num': 1, 'dtype': np.dtype('uint32')}
+
+    BITS_PER_GENE = (COLOR['num'] * COLOR['dtype'].itemsize * 8
+                     + POS['num'] * POS['dtype'].itemsize * 8
+                     + SIZE['num'] * SIZE['dtype'].itemsize * 8)
+
+    def __init__(self, ngenes, target, copying=False):
+        super(ImageChromo, self).__init__(ngenes, ImageChromo.BITS_PER_GENE)
+
+        if not copying:
+            self.target = Image.open(target)
+            self.width, self.height = self.target.size
+            self.image = Image.new('RGB', self.target.size)
+            self.draw = ImageDraw.Draw(self.image)
+
+            self.dir = os.path.join('output', target)
+            shutil.rmtree(self.dir, ignore_errors=True)
+            os.makedirs(self.dir)
+
+    def __deepcopy__(self, memo):
+        new = ImageChromo(self.ngenes, self.target, True)
+
+        new.target = self.target.copy()
+        new.width, new.height = self.target.size
+        new.image = self.image.copy()
+        new.draw = ImageDraw.Draw(new.image)
+
+        new.genes = np.copy(self.genes)
+
+        return new
+
+    def fitness(self):
+        if not self._recalc:
+            return self._fit
+
+        self._recalc = False
+        self.decode()
+
+        dif = ImageChops.difference(self.target, self.image)
+        self._fit = sum(ImageStat.Stat(dif).sum2)
+        return self._fit
+
+    def decode(self):
+        self.draw.rectangle(((0, 0), self.image.size), fill=(0, 0, 0))
+        c_data = ImageChromo.COLOR
+        p_data = ImageChromo.POS
+        s_data = ImageChromo.SIZE
+
+        c_size = c_data['num'] * c_data['dtype'].itemsize * 8
+        p_size = p_data['num'] * p_data['dtype'].itemsize * 8
+
+        for gene in np.split(self.genes, self.ngenes):
+            color = self._decode_gene(gene[:c_size], **c_data)
+            pos = self._decode_gene(gene[c_size + 1:c_size + p_size], **p_data)
+            size = self._decode_gene(gene[c_size + p_size + 1:], **s_data)
+
+            pos = (pos.astype(float) / np.iinfo(p_data['dtype']).max) * self.image.size
+            size = (size.astype(float) / np.iinfo(s_data['dtype']).max) * min(self.image.size)
+
+            self.draw.ellipse((tuple(pos), tuple(pos + size)),
+                              fill=tuple(color))
+
+    @staticmethod
+    def _decode_gene(gene, num, dtype):
+        bytes = np.packbits(gene).tobytes()
+        decoded = np.fromstring(bytes, dtype=dtype, count=num)
+        return decoded if len(decoded) > 1 else decoded[0]
+
+    def log(self, gen):
+        path = os.path.join(self.dir, str(gen) + '.png')
+        self.image.save(path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('target')
+    parser.add_argument('-m', '--max_gen', default=None, type=int)
+    parser.add_argument('-i', '--interval', default=20, type=int)
+    parser.add_argument('-p', '--progress', action='store_true')
+    parser.add_argument('-s', '--select_ratio', default=0.3, type=float)
+    parser.add_argument('-c', '--pool_size', default=30, type=int)
+    parser.add_argument('-g', '--ngenes', default=300, type=int)
+
+    kwargs = vars(parser.parse_args())
+
+    ga = ga.GenAlg(ImageChromo, **kwargs)
+    answer = ga.evolve()
